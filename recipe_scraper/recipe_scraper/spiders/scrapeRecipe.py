12d1653e69eb8c62815357b2477f9c16a06edf5b
lunarie16 <maluna.menke@web.de>
2022-10-07 11:35:10 +0200

    ingridient scraper for every recipe done

--- recipe_scraper/recipe_scraper/spiders/scrapeRecipe.py
+++ recipe_scraper/recipe_scraper/spiders/scrapeRecipe.py
@@ -1,25 +1,40 @@
 import scrapy
 from bs4 import BeautifulSoup
+import jsonlines
 
 
 class ScrapeRecipeSpider(scrapy.Spider):
     name = "scrapeRecipe"
 
+    def __init__(self):
+        self.recipeName = ''
+        self.recipeURL = ''
+
     def start_requests(self):
         f = open('names_urls.txt')
         lines = f.readlines()
-        # for line in lines:
-        #     url = line.split(';')[1]
-        #     yield scrapy.Request(url=f'{url}', callback=self.parse)
-        url = lines[0].split(';')[1]
-        yield scrapy.Request(url=f'{url}', callback=self.parse)
+        for line in lines:
+            self.recipeName, self.recipeURL = line.split(';')
+            self.recipeURL = self.recipeURL.replace('\n', '')
+            yield scrapy.Request(url=f'{self.recipeURL}', callback=self.parse)
+
 
     def parse(self, response):
         bs = BeautifulSoup(response.body, 'lxml')
-        div = bs.find_all("wprm-recipe-ingredients")
-        print(div)
-        # f = open("names_urls.txt", 'a')
-        # for entry in div:
-        #     url = entry.get('href')
-        #     recipeName = entry.get_text()
-        #     f.write(f'{recipeName};{url}\n')
+        div = bs.find_all('div', class_="wprm-recipe-ingredient-group")
+        ingredients = []
+        for d in div:
+            lists = d.find_all('li')
+            for ing in lists:
+                spans = ing.find_all('span')
+                ingredient = []
+                for i, span in enumerate(spans):
+                    if i != 0:
+                        ingredient.append(span.get_text().strip())
+                ingredients.append(" ".join(ingredient).strip())
+        with jsonlines.open('../RecipeWithIngredients.jsonl', mode='a') as writer:
+            writer.write({
+                'name': self.recipeName,
+                'url': self.recipeURL,
+                'ingredients': ingredients
+                 })
