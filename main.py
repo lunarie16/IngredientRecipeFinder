a52439fabed62ba562e36aea0f77f9c0d43ace84
lunarie16 <maluna.menke@web.de>
2022-10-07 23:09:35 +0200

    search now enabled for ingridient search in recipe, can get introductions for it if wanted

--- main.py
+++ main.py
@@ -1,16 +1,62 @@
-# This is a sample Python script.
+import json
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+file = open('RecipeWithIngredients.jsonl', 'r')
+data = file.readlines()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def search(searchTerms: list, amount_results: int = 5):
+    result = []
+    for d in data:
+        d = json.loads(d)
+        ingredients = ' '.join(d['ingredients']).lower()
+        score = 0
+        found = []
+        for term in searchTerms:
+            ingredient = term.lower().strip()
+            if ingredient in ingredients:
+                score += 1
+                found.append(ingredient.capitalize())
+        finalScore = score / len(searchTerms)
+        if finalScore > 0:
+            d['score'] = finalScore
+            d['foundIngredients'] = found
+            result.append(d)
+    result = sorted(result, key=lambda d: d['score'], reverse=True)
+    if not result:
+        print(f'Sorry, seems like we did not find ANY recipe with your searched terms: "{", ".join(searchTerms)}"')
+    else:
+        printResults(searchTerms, result, amount_results)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def printResults(searchTerms: list, results: list, amount_results: int):
+    results = results[:amount_results]
+    print(
+        f'Your searched terms: "{", ".join(searchTerms)}" have been found in the following {amount_results} recipes:\n')
+    for i, result in enumerate(results):
+        print(
+            f'{i + 1}. {int(result["score"] * 100)}% match   {result["name"]}  ({result["url"]})\n    -> containing "{", ".join(result["foundIngredients"])}"\n')
+    print()
+
+    more = input('Do you want some introductions for a recipe? Type the given number for the recipe or enter "no":\n>')
+    if more.lower() == 'no':
+        exit()
+    try:
+        number = int(more)
+        number = number - 1
+        print('\n')
+        print(len(f'Introductions for "{results[number]["name"].upper()}":') * '-')
+        print(f'Introductions for "{results[number]["name"].upper()}":')
+        print(len(f'Introductions for "{results[number]["name"].upper()}":') * '-', '\n')
+        for j, inst in enumerate(results[number]['instructions']):
+            inst = inst.replace('.', '.\n     ')
+            print(f"{j + 1}.    {inst.strip()}\n")
+
+    except Exception as e:
+        print(e)
+        print("Sorry, it seems like this wasn't a number or was exceeding your results index")
+
+
+if __name__ == "__main__":
+    searchFor = input("Please enter your search terms (comma seperated):\n> ")
+    searchFor = searchFor.split(',')
+    search(searchFor)
