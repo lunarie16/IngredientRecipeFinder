251805b4746fb51ead26254942e5b6cc8b4446c0
lunarie16 <maluna.menke@web.de>
2022-12-08 10:41:16 +0100

    keeping ingredients as split lists for better search

--- recipe_scraper/recipe_scraper/spiders/mainPage.py
+++ recipe_scraper/recipe_scraper/spiders/mainPage.py
@@ -6,17 +6,17 @@ class MainPageSpider(scrapy.Spider):
     name = "mainPage"
 
     def start_requests(self):
-        url = 'https://biancazapatka.com/de/rezepte/hauptgerichte/page/'
-
+        # url = 'https://biancazapatka.com/de/rezepte/hauptgerichte/page/'
+        url = 'https://biancazapatka.com/en/recipes/lunch-dinner-en/page/'
         # while self.continueCrawling:
         # 26
-        for i in range(1, 26):
-            yield scrapy.Request(url=f'{url}{i}', callback=self.parse)
+        for i in range(1, 30):
+            yield scrapy.Request(url=f'{url}{i}/', callback=self.parse)
 
     def parse(self, response):
         bs = BeautifulSoup(response.body, 'lxml')
         div = bs.find_all(rel="bookmark")
-        f = open("names_urls.txt", 'w')
+        f = open("names_urls_en.txt", 'a')
         for entry in div:
             url = entry.get('href')
             recipeName = entry.get_text()
--- recipe_scraper/recipe_scraper/spiders/scrapeRecipe.py
+++ recipe_scraper/recipe_scraper/spiders/scrapeRecipe.py
@@ -6,7 +6,6 @@ import jsonlines
 class ScrapeRecipeSpider(scrapy.Spider):
     name = "scrapeRecipe"
 
-
     def start_requests(self):
         f = open('names_urls.txt')
         lines = f.readlines()
@@ -20,14 +19,16 @@ class ScrapeRecipeSpider(scrapy.Spider):
         div = bs.find_all('div', class_="wprm-recipe-ingredient-group")
         ingredients = []
         for d in div:
-            lists = d.find_all('li')
+            lists = d.find_all('li', class_="wprm-recipe-ingredient")
             for ing in lists:
                 spans = ing.find_all('span')
                 ingredient = []
                 for i, span in enumerate(spans):
                     if i != 0:
                         ingredient.append(span.get_text().strip())
-                ingredients.append(" ".join(ingredient).strip())
+                ingredient = list(filter(None, ingredient))
+                ingredient = [words for segments in ingredient for words in segments.split()]
+                ingredients.append(ingredient)
 
         div2 = bs.find_all('li', class_="wprm-recipe-instruction")
         instructions = []
@@ -35,7 +36,8 @@ class ScrapeRecipeSpider(scrapy.Spider):
             span = d.get_text()
             if span:
                 instructions.append(span.strip())
-        with jsonlines.open('../RecipeWithIngredients.jsonl', mode='w') as writer:
+
+        with jsonlines.open('../RecipeWithIngredients.jsonl', mode='a') as writer:
             writer.write({
                 'name': name,
                 'url': url,
